70b360ff988640c88dca48a3473bc4c8ee3148fe
VismayShende <shendevismay@gmail.com>
2025-03-26 15:53:28 +0530

    Java COdes

---
+++ boy.java
@@ -0,0 +1,13 @@
+//creating boy class which has 3 data members name,height,sports .creating an object with new keyword(no intialization)
+class Boy {
+    String name;
+    int height;
+    String sports;
+
+    public static void main(String[] args) {
+        Boy b1 = new Boy();
+        System.out.println("Name: " + b1.name);
+        System.out.println("Height: " + b1.height);
+        System.out.println("Sports: " + b1.sports);
+    }
+}
